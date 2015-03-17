e7ec50abf7486180b8a1f647c294a45688bea699
Patrick D'appollonio <patrick@dappollonio.us>
2015-03-17 18:44:33 -0400

    Add Godoc.

--- auth.go
+++ auth.go
@@ -1,3 +1,18 @@
+/*
+goji-auth provides an easy and simple solution to use a basic HTTP Auth.
+You need to know, beforehand, that Basic Auth is considered insecure because
+both the username and password are encoded and not encrypted (meaning,
+anyone with enough knowlegde could retrieve them easily), so it's important
+to put the website where you're going to use your password under an SSL
+connection to prevent eavesdropping.
+
+This package only works with the [Goji package](https://github.com/zenazn/goji)
+and not plain `net/http`. There are several packages to use with a plain `net/http`
+setup. You could also use it with the [cji package](https://github.com/pressly/cji)
+so you can mix and match it with your routes.
+
+Examples are provided in the [Github repository](https://github.com/patrickdappollonio/goji-auth).
+*/
 package auth
 
 import (
@@ -21,6 +36,7 @@ type AuthConfig struct {
 	UnauthorizedMessage string
 }
 
+// Handy shorthand to create an AuthConfig with username and password.
 func WithUserPass(username, password string) AuthConfig {
 	return AuthConfig{
 		Username:            username,
@@ -30,6 +46,8 @@ func WithUserPass(username, password string) AuthConfig {
 	}
 }
 
+// Handy shorthand to create an AuthConfig with username, password and a custom
+// message.
 func WithUserPassMessage(username, password, message string) AuthConfig {
 	return AuthConfig{
 		Username:            username,
@@ -39,6 +57,8 @@ func WithUserPassMessage(username, password, message string) AuthConfig {
 	}
 }
 
+// Base Goji middleware where you can pass an AuthConfig struct or one of the predefined functions
+// with username and password and/or custom message to perform an authentication.
 func Auth(config AuthConfig) func(c *web.C, h http.Handler) http.Handler {
 	if strings.TrimSpace(config.Username) == "" || strings.TrimSpace(config.Password) == "" {
 		panic("auth: username and/or password are empty")
